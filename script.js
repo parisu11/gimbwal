2388ce8bd6f2e37bf55a49bab7b62291c3798214
parisu11 <zwankyy08@gmail.com>
2025-03-02 22:06:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function getFarPosition(containerRect, windowWidth, windowHeight, buttonWidth, b
 }
 
 yesBtn.addEventListener('click', () => {
-    title.innerHTML = "HORE!! i knew you would say vqsghvwegdsbjs";
+    title.innerHTML = "YEAY!! i know you would say vqsghvwegdsbjs";
     document.querySelector('img').src = "https://media.tenor.com/gUiu1zyxfzYAAAAi/good-night-kiss-kiss.gif";
     noBtn.style.display = 'none';
     yesBtn.style.display = 'none';
